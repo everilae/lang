265f1db22410d0b5f2ef63fbfb5b0e44d3beda2d
saarni <saarni@gmail.com>
2015-03-25 10:05:50 +0200

    add trie_new assert

--- tests/test_trie.c
+++ tests/test_trie.c
@@ -19,10 +19,13 @@ int
 main(int argc, char* argv[])
 {
 	Trie t = trie_new();
+	assert(t);
+
 	for (size_t i = 0; pairs[i][0]; i++) {
 		assert(trie_set(t, pairs[i][0], pairs[i][1]) != NULL);
 		assert(trie_get(t, pairs[i][0]) == pairs[i][1]);
 	}
+
 	assert(trie_get(t, "SHOULDNOTBE") == NULL);
 	trie_repr(t, NULL);
 	trie_delete(t);
